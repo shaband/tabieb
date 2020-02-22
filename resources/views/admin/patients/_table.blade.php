27db2600e046371fac254160877511a1aac8fc0f
shaband <mahmoud.shaband@roqay.com.kw>
2020-02-22 18:50:09 +0200

    working on patients

---
+++ resources/views/admin/patients/_table.blade.php
@@ -0,0 +1,42 @@
+<table id="datatable-buttons" class="table table-striped table-bordered dt-responsive nowrap">
+    <thead>
+    <tr>
+        <th>{!! __("Name ") !!}</th>
+        <th>{!! __("Title") !!}</th>
+        <th>{!! __("Email") !!}</th>
+        <th>{!! __("Phone") !!}</th>
+        <th>{!! __("Price") !!}</th>
+        <th>{!! __("Status") !!}</th>
+
+        <th>{!! __('Actions') !!}</th>
+
+    </tr>
+    </thead>
+    @foreach($doctors as $doctor)
+        <tr>
+            <td>{!! $doctor->name !!}</td>
+            <td>{!! $doctor->title !!}</td>
+            <td>{!! $doctor->email !!}</td>
+            <td>{!! $doctor->phone !!}</td>
+            <td>{!! $doctor->price !!}</td>
+            <td>
+                @component('admin.partials._block',
+                            [
+                             'id'=>$doctor->id,
+                             'blocked_at'=>$doctor->blocked_at,
+                             'routeName'=>'doctors',
+                             'textMessage'=>__('This Doctor Would Not Be Available For Patients')
+                             ])
+                @endcomponent
+
+            </td>
+            <td>
+                @component('admin.partials._action_buttons',['routeName'=>'doctors','id'=>$doctor->id])
+                @endcomponent
+            </td>
+        </tr>
+    @endforeach
+    <tbody>
+
+    </tbody>
+</table>
---
+++ resources/views/admin/patients/index.blade.php
@@ -0,0 +1,50 @@
+@extends('admin.layouts.app')
+@section('title') {!! __("Doctors") !!} @endsection
+
+@section('content')
+
+    <div class="row">
+        <div class="col-12">
+            <div class="card-box">
+                <h4 class="mt-0 header-title d-inline">@yield('title')</h4>
+                <div class="mt-0 header-title float-right  d-inline">
+
+                    <a class="btn btn-info text-white" href="{!! route('admin.doctors.create') !!}">
+                        <i class="fas fa-plus"></i> {!! __("New")!!}</a>
+                </div>
+                <div class="pt-4">
+
+
+                    <nav>
+                        <div class="nav nav-tabs" id="doctors-tab" role="tablist">
+                            <a class="nav-item nav-link active" id="open-doctors-tab" data-toggle="tab"
+                               href="#open-doctor" role="tab" aria-controls="open-doctor"
+                               aria-selected="true">{!! __("Open Doctors") !!}</a>
+                            <a class="nav-item nav-link" id="sub-cat-tab" data-toggle="tab" href="#blocked-doctor"
+                               role="tab" aria-controls="blocked-doctor"
+                               aria-selected="false">{!! __("Blocked Doctors") !!}</a>
+                        </div>
+                    </nav>
+                    <div class="tab-content" id="doctors-tabContent">
+                        <div class="tab-pane fade show active" id="open-doctor" role="tabpanel"
+                             aria-labelledby="open-doctors-tab">
+
+                            @include('admin.doctors._table',['doctors'=>$open_doctors])
+
+
+                        </div>
+                        <div class="tab-pane fade" id="blocked-doctor" role="tabpanel" aria-labelledby="sub-cat-tab">
+
+                            @include('admin.doctors._table',['doctors'=>$blocked_doctors])
+
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+
+@endsection
