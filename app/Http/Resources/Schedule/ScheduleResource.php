3d933c8876e7bb0fef26e362b422bcd71813c6f7
Wael Moharram <wael.moharram@gmail.com>
2020-06-20 16:45:33 +0200

    remove time validation

--- app/Http/Resources/Schedule/ScheduleResource.php
+++ app/Http/Resources/Schedule/ScheduleResource.php
@@ -27,8 +27,8 @@ class ScheduleResource extends JsonResource
             'name' => $name,
             'doctor' => new DoctorResource($this->whenLoaded('doctor')),
             'day' => days()[$this->day],
-            'from_time' => CarbonImmutable::parse($this->from_time)->format('H:i A'),
-            'to_time' => CarbonImmutable::parse($this->to_time)->format('H:i A'),
+            'from_time' => CarbonImmutable::parse($this->from_time)->format('h:i A'),
+            'to_time' => CarbonImmutable::parse($this->to_time)->format('h:i A'),
             'reservations' => ReservationResource::collection($this->whenLoaded('reservations'))
         ];
     }
